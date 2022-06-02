adbc9a7c6bcf1634dbaa46b37cb20a09ea3606a7
jumpei-jp <junpeiyamaoka0516@gmail.com>
2022-06-02 15:30:24 +0900

    チャートの表示が完了

--- text.js
+++ text.js
@@ -6,28 +6,8 @@ const HighchartsWrapper = (props) => {
    console.log(props.chartData); // this is always correct
 
     let options = {
-      chart: {
-        renderTo: 'container',
-        type: 'bar'
-      },
-      title: {
-          text: 'Fruit Consumption'
-      },
-      xAxis: {
-          categories: ['Apples', 'Bananas', 'Oranges']
-      },
-      yAxis: {
-          title: {
-              text: 'Fruit eaten'
-          }
-      },
-      series: [{
-          name: 'Jane',
-          data: [1, 0, 4]
-      }, {
-          name: 'John',
-          data: [5, 7, 3]
-      }]
+        title: { text: 'My chart' },
+        series: props.chartData
     }
 
     return(
